c34dc0845bc5115360a2211ec50253ed8555e19e
Matheus Freitas <47475180+MathFreitaS@users.noreply.github.com>
2019-08-05 10:18:57 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+/*Função que adicionará o que foi escrito no input
+para uma lista ordenada, caso nada for escrito, pedirá uma tarefa*/
+function toDoList() {
+	if (document.getElementById("toDoInput").value == ""){
+		alert("Escreva uma tarefa")
+	}
+		else {
+	var item = document.getElementById("toDoInput").value
+	var text = document.createTextNode(item)
+	var newItem = document.createElement("li")
+	newItem.appendChild(text)
+	document.getElementById("toDoList").appendChild(newItem)
+	document.getElementById("toDoInput").value = ""
+
+/*variável responsável por criar um botão ao lado da nova tarefa
+com a função de quando clicado, removerá o que foi criado acima*/
+
+	var removeTask = document.createElement("input");
+	    removeTask.setAttribute("type", "button");
+	    removeTask.setAttribute("value", "X");
+	    removeTask.setAttribute("id", "removeButton");
+	    removeTask.addEventListener("click", function(e) {
+	        text.parentNode.removeChild(text)
+	        newItem.parentNode.removeChild(newItem);
+	    });
+	    newItem.appendChild(removeTask);
+
+	}
+
+
+}
+
